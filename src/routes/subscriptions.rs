20cebed5099bea5a88b528ba36c82130f13de672
James Daly <jamsiedaly@gmail.com>
2021-07-13 14:49:54 +0200

    Adding a manual version of tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,17 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -27,8 +38,16 @@ pub async fn subscribe(
     .execute(connection.get_ref())
     .await
     .map_err(|e| {
-        eprintln!("Failed to execute query: {}", e);
+        log::error!(
+            "request_id {} - Failed to execute query: {:?}",
+            request_id,
+            e
+        );
         HttpResponse::InternalServerError().finish()
     })?;
+    log::info!(
+        "request_id {} - New subscriber details have been saved",
+        request_id
+    );
     Ok(HttpResponse::Ok().finish())
 }
